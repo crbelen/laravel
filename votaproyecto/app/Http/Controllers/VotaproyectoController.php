05c1f776a7463b25dd41cb2421264dc85f092e27
crbel <crbelen28@gmail.com>
2023-10-28 16:46:42 +0200

    281023votos

---
+++ votaproyecto/app/Http/Controllers/VotaproyectoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\votaproyecto;
+use Illuminate\Http\Request;
+
+class VotaproyectoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(votaproyecto $votaproyecto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(votaproyecto $votaproyecto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, votaproyecto $votaproyecto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(votaproyecto $votaproyecto)
+    {
+        //
+    }
+}
---
+++ votaproyecto/resources/views/welcome.blade.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body class="p-3 m-0 border-0 bd-example m-0 border-0">
+
+    <!-- barra de navegacion-->
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Barra de navegación</font></font></span>
+          
+        <!-- enlaces de navegación-->
+            <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Activo</font></font></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Enlace</font></font></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Enlace</font></font></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Desactivado</font></font></a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- contenido grid-->
+    <div class="container">
+      <div class="row g-6"><!-- espacio entre filas-->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen1.jpg" class="card-img-top" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 1</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 1</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 3</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 3</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+
+
+      <div class="row g-6"><!-- espacio entre filas-->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 4</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 4</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <div class="row g-6"><!-- espacio entre filas-->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>  
+            
+        <div class="row g-6"><!-- espacio entre filas-->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <img src="imagen2.jpg" class="card-img-top" alt="Imagen 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Título 2</h5>
+                        <p class="card-text">Texto de ejemplo para la cuadrícula.</p>
+                        <a href="#" class="btn btn-primary">Botón 2</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
